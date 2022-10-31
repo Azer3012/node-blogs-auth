39387f58ef8c61efecdf220d8367aca1f7d6def3
azer <azer@azers-MacBook-Pro.local>
2022-10-31 20:03:16 +0400

    jwt token usage auth handling

--- routes/blog.js
+++ routes/blog.js
@@ -1,69 +1,74 @@
-import express from 'express'
-import Blog from "../models/blog.js"
-import Session from '../models/session.js'
-const router=express.Router()
+import express from "express";
+import Blog from "../models/blog.js";
+import Session from "../models/session.js";
+import jwt from "jsonwebtoken";
+const router = express.Router();
 
-router.use(async(req,res,next)=>{
-    const token=req.headers.token || req.body.token
+router.use(async (req, res, next) => {
+  const token = req.headers['authorization'];
 
-    console.log(token);
-    if(token){
-        const currentSession=await Session.findOne({accessToken:token}).populate('user').exec()
-        console.log(currentSession);
-        if(currentSession.expiresAt.getTime() < Date.now()){
-            res.status(401).send('Your session has expired!')
-        }
-        else{
-            req.user=currentSession.user
-            next()
-        }
-        
-    }else{
+  console.log(token);
+  if (token) {
+    const data = jwt.verify(token,process.env.JWT_SECRET_KEY, (error, decoded) => {
+      if (error) {
         res.status(401).send({
-            message:"UnAuthorized request"
-        })
-    }
-})
+          message: error,
+        });
+        return;
+      }
+
+      if (decoded.exp.expiresAt < (Date.now()/1000)) {
+        res.status(401).send("Your session has expired!");
+      } else {
+        req.user = decoded.data;
+        next();
+      }
+    });
+  } else {
+    res.status(401).send({
+      message: "UnAuthorized request",
+    });
+  }
+});
 
 //all blogs
-router.get('/blogs',async (req,res)=>{
-    const blogs=await Blog.find().populate("author").exec()
-    res.status(200).send(blogs)
-})
+router.get("/blogs", async (req, res) => {
+  const blogs = await Blog.find().populate("author").exec();
+  res.status(200).send(blogs);
+});
 
 //selected blog
-router.get('/blogs/:id',async (req,res)=>{
-    const blog=await Blog.findById(req.params.id).populate("author").exec()
-    res.status(200).send(blog)
-})
+router.get("/blogs/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id).populate("author").exec();
+  res.status(200).send(blog);
+});
 
 //post
-router.post('/blogs',async (req,res)=>{
-    const blog=new Blog({
-        ...req.body,
-        author:req.user._id
-    })
-   await blog.save()
-   res.status(201).send()
-})
+router.post("/blogs", async (req, res) => {
+  const blog = new Blog({
+    ...req.body,
+    author: req.user._id,
+  });
+  await blog.save();
+  res.status(201).send();
+});
 
 //update blog
-router.put('/blogs/:id',async (req,res)=>{
-await Blog.findByIdAndUpdate(req.params.id,req.body)
-res.status(200).send()
-})
+router.put("/blogs/:id", async (req, res) => {
+  await Blog.findByIdAndUpdate(req.params.id, req.body);
+  res.status(200).send();
+});
 //delete blog
-router.delete('/blogs/:id',async (req,res)=>{
+router.delete("/blogs/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
 
-    const blog=await Blog.findById(req.params.id)
-
-    if(blog.author!==req.user._id){
-        res.status(403).send({
-            message:"You cannot delete others people's posts"
-        })
-    }
-    await Blog.findByIdAndDelete(req.params.id)
-    res.status(200).send()
-})
+  if (blog.author !== req.user._id) {
+    res.status(403).send({
+      message: "You cannot delete others people's posts",
+    });
+  }
+  await Blog.findByIdAndDelete(req.params.id);
+  res.status(200).send();
+});
 
 export default router;

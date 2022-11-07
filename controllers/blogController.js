87f93796b912d609caf13882ddc64e9f1de4bccb
azer <azer@azers-MacBook-Pro.local>
2022-11-07 22:27:12 +0400

    created https server and ssl certificate

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,35 +1,47 @@
+import Blog from "../models/blog.js";
 
-import Blog from '../models/blog.js'
-
-const getAllBlogs=async (req, res) => {
+const getAllBlogs = async (req, res, next) => {
+  try {
     const blogs = await Blog.find().populate("author").exec();
     res.status(200).send(blogs);
-}
-
-const selectedBlog= async (req, res,next) => {
-    try {
-      const blog = await Blog.findById(req.params.id).populate("author").exec();
-      res.status(200).send(blog);
-    } catch (error) {
-      next(new Error("shit happens"))
-    }
+  } catch (error) {
+    next(error);
   }
-const createNewBlog=async (req, res) => {
+};
+
+const selectedBlog = async (req, res, next) => {
+  try {
+    const blog = await Blog.findById(req.params.id).populate("author").exec();
+    res.status(200).send(blog);
+  } catch (error) {
+    next(error);
+  }
+};
+const createNewBlog = async (req, res, next) => {
+  try {
     const blog = new Blog({
       ...req.body,
       author: req.user._id,
     });
     await blog.save();
     res.status(201).send();
+  } catch (error) {
+    next(error);
   }
-const updateBlog=async (req, res) => {
+};
+const updateBlog = async (req, res, next) => {
+  try {
     await Blog.findByIdAndUpdate(req.params.id, req.body);
     res.status(200).send();
+  } catch (error) {
+    next(error);
   }
+};
 
-const deleteBlog=async (req, res) => {
+const deleteBlog = async (req, res, next) => {
+  try {
     const blog = await Blog.findById(req.params.id);
-  
+
     if (blog.author !== req.user._id) {
       res.status(403).send({
         message: "You cannot delete others people's posts",
@@ -37,7 +49,9 @@ const deleteBlog=async (req, res) => {
     }
     await Blog.findByIdAndDelete(req.params.id);
     res.status(200).send();
+  } catch (error) {
+    next(error);
   }
+};
 
-
-export  {getAllBlogs,selectedBlog,createNewBlog,updateBlog,deleteBlog}
+export { getAllBlogs, selectedBlog, createNewBlog, updateBlog, deleteBlog };
